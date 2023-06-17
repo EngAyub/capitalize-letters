3bafb79b4410e1cfdba4713ee9e5dbae44fb2158
ENG AYUUB <moztaphem9@gmal.com>
2023-06-17 20:19:31 +0300

    capitalize- letter done

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Good Luck 💪🏾
+
+function makeCapital(str) {
+    let words = str.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    return words.join(" ");
+  }
+  console.log(makeCapital("my name is ali"));
+  console.log(makeCapital("fikrcamp bootcamp"));
+  console.log(makeCapital("i live In hargeisa"));
